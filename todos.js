9d1a522e7c5c04f957ae7dc71c0fa69bd4302899
Tyler Garcia <t-garcia15@hotmail.com>
2016-06-05 18:06:03 -0700

    modified todos

--- todos.js
+++ todos.js
@@ -2,27 +2,9 @@ var express = require('express');
 
 var router = express.Router();
 
-var todoItems = [
-		{ id: 1, desc: 'foo' },
-		{ id: 2, desc: 'bar' },
-		{ id: 3, desc: 'baz' }
-	];
-
 	router.get('/', function(req, res){
-	res.render('index', {
-		title: 'My App',
-		items: todoItems
-	});
-});
-
-router.post('/add', function(req, res) {
-	var newItem = req.body.newItem;
+	res.render('index');
 	
-	todoItems.push({
-		id:todoItems.length + 1,
-		desc: newItem
-	});
-	res.redirect('/');
 });
 
 module.exports = router;
