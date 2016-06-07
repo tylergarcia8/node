7675cdf5c130fa5dce09f6ac4695151c81289fa6
Tyler Garcia <t-garcia15@hotmail.com>
2016-06-06 18:08:33 -0700

    other routes created

--- todos.js
+++ todos.js
@@ -1,10 +1,16 @@
 var express = require('express');
-
+var app = express();
 var router = express.Router();
 
-	router.get('/', function(req, res){
+	router.get('/', function(req, res) {
 	res.render('index');
-	
 });
+	router.get('/users', function(req, res) {
+	res.render('users',);
+});
+	router.get('/contact', function(req, res) {
+	res.render('contact');
+});
+
 
 module.exports = router;
